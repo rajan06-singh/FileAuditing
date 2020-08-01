3b70d31fba561faa54f3e9b3be17bf6cef5cc657
Rajan Singh <rajan06.singh@gmail.com>
2020-08-01 10:06:02 -0400

    Update AppleWatching.sh

--- AppleWatching.sh
+++ AppleWatching.sh
@@ -1,14 +1,12 @@
 #this is a shell code to detect files patterns live time on MacBook or any Apple devices. 
+#/bin/bash/
 
-#!/bin/bash 
-
-watchmedo shell-command \ 
-         --patterns='*' \ 
-         --directories="specify the directory path or it will be default ." \
-         --recursive \
-         --ignore_directories \
-         --command=' echo "${watch_object}: \c"; 
-                          echo "${watch_event_type}: \c";
-                               echo "${watch_src_path}: \c"; 
-                                   defaults read "/Library/Preferences/SystemConfiguration/com.apple.smb.server" NetBIOSName' \
-         --wait
+watchmedo shell-command \
+		--patterns="*" \
+		--recursive \
+		--ignore-directories \
+		--command='time=$(date +"%r"); echo "$time : \c"; 
+				 	echo "${watch_object}: \c" ;         
+                                                echo "${watch_event_type}: \c" ;
+                                                        echo "${watch_src_path}: \c" ;
+                                         			echo "$HOSTNAME" ' >> Output.txt 
